ed42e0206d6ef312723c61adb4a35349e6b6b599
matteoVALENTINI <matteo.valentini@efrei.net>
2024-02-09 23:04:13 +0100

    update nackend

--- mlops_nba/backend.py
+++ mlops_nba/backend.py
@@ -2,31 +2,30 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
-import csv
+
+# Vos fonctions personnalisées (assurez-vous qu'elles sont correctement définies)
 from functions import read_first_column_of_csv, read_schedule
 
 app = FastAPI()
 
+# Configurez le chemin pour servir les fichiers statiques.
 app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
 
-# Obtention du chemin absolu du répertoire courant où se trouve backend.py
+# BASE_DIR doit pointer vers le répertoire qui contient le dossier 'frontend'.
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Configuration de Jinja2 pour utiliser le chemin absolu des templates
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "frontend"))
-
 
 @app.get("/")
 async def show_dropdown(request: Request):
-    file_path_schedule = f"../dataset/NBASchedule23-24.csv"
-    options = read_first_column_of_csv(file_path_schedule)  # Ajustez le chemin d'accès au fichier CSV
+    # Mettez à jour le chemin d'accès pour pointer vers le fichier csv correctement.
+    file_path_schedule = os.path.join(BASE_DIR, "../dataset/NBASchedule23-24.csv")
+    options = read_first_column_of_csv(file_path_schedule)
     return templates.TemplateResponse("firstpage.html", {"request": request, "options": options})
 
-
-# Nouvelle route pour récupérer les matchs basée sur une date
 @app.get("/matches/{selected_date}")
 async def get_matches_for_date(selected_date: str):
-    file_path_schedule = os.path.join(BASE_DIR, "../dataset/NBASchedule23-24.csv")  # Ajustez le chemin
+    # Même chose ici, assurez-vous que le chemin vers le fichier csv est correct.
+    file_path_schedule = os.path.join(BASE_DIR, "../dataset/NBASchedule23-24.csv")
     schedule = read_schedule(file_path_schedule)
     matches = schedule.get(selected_date, [])
     return {"matches": matches}
